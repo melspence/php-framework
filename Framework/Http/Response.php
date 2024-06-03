2bd653de4548859c5f354788448acd700375fced
melspence <melvillespence@hamnavoesolutions.com>
2024-06-03 23:46:57 +0100

    Add HTTP Request and Response classes
    Introduced HTTP Request and Response classes for managing HTTP requests and responses. Also updated the public index file to utilize these classes and adjusted the composer.json file to add autoload information. Finalized composer.lock file.

---
+++ Framework/Http/Response.php
@@ -0,0 +1,23 @@
+<?php
+	
+	namespace MelSpence\Framework\Http;
+	
+	class Response
+	{
+		public function __construct(
+			private ?string $content = '',
+			private int $status = 200,
+			private array $headers = [],
+		)
+		{
+		
+		}
+		
+		public function send(): void
+		{
+			echo $this->content;
+		}
+		
+		
+		
+	}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
-<?php declare(strict_types=1); // public/index.php
+<?php
+	declare(strict_types=1); // public/index.php
+	use MelSpence\Framework\Http\Request;
+	use MelSpence\Framework\Http\Response;
+	
+	require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // request received
+	$request = Request::createFromGlobals();
+	
 
 // perform some logic
 
 // send response (string of content)
-	echo 'Hello World';
+	$content =  '<h1>Hello World</h1>';
+	
+	$response = new Response($content, status: 200, headers: []);
+	
+	$response->send();
