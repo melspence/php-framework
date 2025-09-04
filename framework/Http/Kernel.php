9cf03724a34798183a6e9c27366a3f7be1353972
Melville Spence <melvillespence@hamnavoesolutions.com>
2025-09-04 14:12:19 +0100

    Created basic Kernel class

---
+++ framework/Http/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+    declare(strict_types=1);
+    
+    
+    namespace MelSpence\Framework\Http;
+    
+    class Kernel
+    {
+        public function handle(Request $request): Response
+        {
+            $content = '<h1>Hello World</h1>';
+            return new Response($content);
+            
+        }
+        
+    }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
     
     declare(strict_types=1);
     
+    use MelSpence\Framework\Http\Kernel;
     use MelSpence\Framework\Http\Request;
     use MelSpence\Framework\Http\Response;
     
@@ -10,13 +11,13 @@
 
 // request received
     $request = Request::createFromGlobals();
-    
+
 
 //perform some logic
 
 //send response
-$content = '<h1>Hello World!</h1>';
 
-$response = new Response(content: $content, status: 200, headers: []);
+    $kernel = new Kernel;
 
+$response = $kernel->handle($request);
 $response->send();
